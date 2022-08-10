252e942fe6c9020f3bb201aa4057228844585b1c
clesiorods <clesiorodrigues96@gmail.com>
2022-08-10 00:10:32 -0300

    Ajuste na base do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { BrowserRouter } from 'react-router-dom'
-import GlobalStyle from './GlobalStyle'
-import { Router } from './Router'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Router />
-    </BrowserRouter>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
